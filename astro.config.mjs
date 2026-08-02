e6df32f8008303a11b3579c2a4e35fadc4ced708
evaevangelisti <eva.evangelisti03@gmail.com>
2026-08-02 12:10:03 +0200

    style: switch indentation to tabs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://evaevangelisti.dev",
-    build: {
-        inlineStylesheets: "always",
-    },
+	site: "https://evaevangelisti.me",
+	build: {
+		inlineStylesheets: "always",
+	},
 });
--- src/site.config.ts
+++ src/site.config.ts
@@ -1,11 +1,11 @@
 export const site = {
-    name: "Eva Evangelisti",
-    role: "Computer Science Graduate & Philosophy Student",
-    description:
-        "Computer Science graduate with a background in Philosophy, interested in Natural Language Processing.",
-    email: "hi@evaevangelisti.dev",
-    socials: {
-        github: "https://github.com/evaevangelisti",
-        linkedin: "https://www.linkedin.com/in/eva-evangelisti",
-    },
+	name: "Eva Evangelisti",
+	role: "Computer Science Graduate & Philosophy Student",
+	description:
+		"Computer Science graduate with a background in Philosophy, interested in Natural Language Processing.",
+	email: "hi@evaevangelisti.me",
+	socials: {
+		github: "https://github.com/evaevangelisti",
+		linkedin: "https://www.linkedin.com/in/eva-evangelisti",
+	},
 } as const;
